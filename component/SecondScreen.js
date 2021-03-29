c89bd6191537adfb889f613706214c87e4f169bc
NguyenPhuongNam99 <nguyenphuongnam06012000@gmail.com>
2021-03-30 00:57:18 +0700

    Nam commit

--- component/SecondScreen.js
+++ component/SecondScreen.js
@@ -1,43 +1,72 @@
 
 import React from 'react';
 import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-  Image,ImageBackground
+    SafeAreaView,
+    ScrollView,
+    StatusBar,
+    StyleSheet,
+    Text,
+    useColorScheme,
+    View,
+    Image, ImageBackground, TextInput
 } from 'react-native';
 
 const SecondScreen = () => {
-  return (
-    <View style={styles.container}>
-        <ImageBackground source={require('../asses/b1.png')} style={styles.image}>
-        <Image
-          style={styles.tinyLogo}
-          source={require('../asses/b2.png')}
-          resizeMode='center'
-        />
-        </ImageBackground>
-       
-    </View>
-  );
+    return (
+        
+        <View style={styles.container}>
+
+            <Image
+                style={styles.image}
+                source={require('../asses/b1.png')}
+            />
+            <Image
+                style={styles.tinyLogo}
+                source={require('../asses/b2.png')}
+            />
+            <Image
+           
+                style={styles.Logo}
+                source={require('../asses/logo.png')}
+            />
+            <TextInput  style={styles.input} keyboardType='numeric'/>
+        </View>
+     
+    );
 };
 
 const styles = StyleSheet.create({
- container:{
-     flex:1
- },
- image:{
-     flex:1
- },
- tinyLogo:{
-     flex:1,
-     width:'100%',
-     height:'100%'
- }
+    container: {
+        flex: 1
+    },
+    image: {
+        flex: 1, resizeMode: 'stretch', width: '100%', height: '100%'
+    },
+    tinyLogo: {
+        flex: 1,
+        width: '100%',
+        height: '100%',
+        position: 'absolute',
+        top: 0,
+        left: 0
+    },
+    Logo: {
+        position: 'absolute',
+        top: '10%',
+        left: '33%',
+       width:132,
+       height:169
+    },
+    input:{
+       
+        position:'absolute',
+        top:'50%',
+        left:'7%',
+        borderWidth:1,
+        width:'88%',
+        borderRadius:30,
+        backgroundColor: 'rgba(0, 0, 0, 0.5)'
+    }
 });
 
 export default SecondScreen;
